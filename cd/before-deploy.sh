03a6488899f7bb1a1e8f24ce96e3c54bb647234c
jsjlewis96 <jsjlewis96@gmail.com>
2018-05-23 17:42:44 +0100

    made deploy scripts executable

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_da73d47e8be3_key -iv $encrypted_da73d47e8be3_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc	-d
+    echo "start openssl"
+	openssl aes-256-cbc -K $encrypted_da73d47e8be3_key -iv $encrypted_da73d47e8be3_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc	-d
+	echo "start gpg import"
     gpg --fast-import cd/codesigning.asc
+	echo "done"
 fi
