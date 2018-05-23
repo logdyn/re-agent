b1ef4d024b5592a790f4fb0ef333fbd46019a62c
jsjlewis96 <jsjlewis96@gmail.com>
2018-05-23 18:33:25 +0100

    redid gpg keys

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     echo "start openssl"
-	openssl aes-256-cbc -K $encrypted_da73d47e8be3_key -iv $encrypted_da73d47e8be3_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc	-d
+	openssl aes-256-cbc -K $encrypted_da73d47e8be3_key -iv $encrypted_da73d47e8be3_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
 	echo "start gpg import"
     gpg --fast-import cd/codesigning.asc
 	echo "done"
